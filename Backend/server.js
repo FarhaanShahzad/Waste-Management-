0cf27ce47ff9e8a487e51c71aeacee4c79ed2075
kkasana <kkasanacoder@gmail.com>
2025-10-19 17:27:42 +0530

    working on authentication part

--- Backend/server.js
+++ Backend/server.js
@@ -1,1 +1,36 @@
-console.log("backend")
+const express = require("express");
+const {connectDB} = require("./config/db")
+const {userRoutes} = require("./routes/user.routes");
+const app = express();
+app.use(express.json());
+require('dotenv').config()
+
+
+const PORT = process.env.PORT;
+connectDB();
+
+
+app.use("/userRoutes" , userRoutes);
+
+
+app.get("/getAll" , (req,res) => {
+   res.status(200).json({msg : "get signup data"});
+})
+
+
+
+app.use((req,res) => {
+   res.status(404).json({msg : "404 not found"});
+})
+
+
+
+app.get('/login' , (req,res) => {
+    res.status(200).json({msg : "please login again github"});
+})
+
+
+
+app.listen(8080 , () => {
+ console.log("server is running on localhost 8080");
+})
